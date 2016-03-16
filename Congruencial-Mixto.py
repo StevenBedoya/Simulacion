f360e6bd7b670b2f6e2e98fbcb3f7b15efe68d04
StevenBedoya <samadhi.mart@gmail.com>
2016-03-15 21:02:09 -0500

    Update Congruencial-Mixto.py

--- Congruencial-Mixto.py
+++ Congruencial-Mixto.py
@@ -1,15 +1,13 @@
-veces = int (raw_input("Ingresa el numero de veces a generar ")) #Se ingresan los datos respectivos
-a = int (raw_input("ingrese la constante multiplicativa "))
+cant = int (raw_input("Cantidad de numeros a generar "))
+c = int (raw_input("Constante aditiva "))
+a = int (raw_input("Constante multiplicativa "))
 semilla= int (raw_input("Ingrese la semilla "))
-c = int (raw_input("Ingrese la constante aditiva "))
-mod = int (raw_input("Ingrese el modulo "))
- 
-print 'n   Xo   (aXo + c) modm   Xn + 1    Numero  '
- 
-for i in range (veces): #desde i hasta el numero que se quiere generar
+n = int (raw_input("Ingrese el modulo "))
+
+print " "
+for i in range (cant):
     i += 1
-    xn = ((a*semilla)+c)%mod    #Formula general
-    div = xn/mod                #Formula general
-    z = float((xn+.0) / (mod+.0))
-    print "%d   %d    (%d  +  %d)/%d     %d        %f  "%(i, semilla, div, xn, mod, xn, z)
-    semilla = xn                #Xn+1 se convierte en la semilla
+    xn = ((a*semilla)+c)%n    
+    z = float((xn+.0) / (n+.0))
+    print (xn, z)
+    semilla = xn
